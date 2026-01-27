6ae5bbb335c8d0e0b1bd0e8d6c2c6faac6f95649
miguelmagalhaes17 <miguelmagalhaes98@gmail.com>
2026-01-27 00:24:39 +0000

    wip

--- src/alu.sv
+++ src/alu.sv
@@ -26,17 +26,17 @@ module alu #(parameter WIDTH = 32)(
 
 always_comb begin
     case(i_alu_op)
-        `OP_ALU_ADD:   o_result = i_a + i_b;
-        `OP_ALU_SUB:   o_result = i_a - i_b;
-        `OP_ALU_AND:   o_result = i_a & i_b;
-        `OP_ALU_OR:    o_result = i_a | i_b;
-        `OP_ALU_XOR:   o_result = i_a ^ i_b;
+        `OP_ALU_ADD:    o_result = i_a + i_b;
+        `OP_ALU_SUB:    o_result = i_a - i_b;
+        `OP_ALU_AND:    o_result = i_a & i_b;
+        `OP_ALU_OR:     o_result = i_a | i_b;
+        `OP_ALU_XOR:    o_result = i_a ^ i_b;
         `OP_ALU_SLTU:   o_result = (i_a < i_b) ? 1 : 0;
-        `OP_ALU_SLT:  o_result = ($signed(i_a) < $signed(i_b)) ? 1 : 0;
-        `OP_ALU_SLL:   o_result = i_a << i_b[4:0];
-        `OP_ALU_SRL:   o_result = i_a >> i_b[4:0];
-        `OP_ALU_SRA:   o_result = $signed(i_a) >>> i_b[4:0]
-    default: o_result = 0;
+        `OP_ALU_SLT:    o_result = ($signed(i_a) < $signed(i_b)) ? 1 : 0;
+        `OP_ALU_SLL:    o_result = i_a << i_b[4:0];
+        `OP_ALU_SRL:    o_result = i_a >> i_b[4:0];
+        `OP_ALU_SRA:    o_result = $signed(i_a) >>> i_b[4:0]
+    default:            o_result = 0;
     endcase
 end
 endmodule
